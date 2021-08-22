08fb3d1f368cc1f9e662cccb0a2fb83be844719a
sabby <sahibpreet82000@gmail.com>
2021-08-23 01:15:20 +0530

    new hamburger icon added

---
+++ hamburger.js
@@ -0,0 +1,11 @@
+function hamburger() {
+  var elems = document.getElementsByClassName("dropdown-content2");
+  for (var i = 0; i < elems.length; i += 1) {
+    if (elems[i].style.display != "block") {
+      elems[i].style.display = "block";
+      continue;
+    } else {
+      elems[i].style.display = "none";
+    }
+  }
+}
