368afcdc0d88ff0c179de223f84226eb2a5a4e8e
sabby <sahibpreet82000@gmail.com>
2021-09-07 22:30:01 +0530

    new animation added

--- javascript/scriptfile.js
+++ javascript/scriptfile.js
@@ -83,4 +83,25 @@ function hovertext3() {
   ho.style.display = "block";
 }
 
-// for product caterlog
+// for animations
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".section-content");
+  var position = element.getBoundingClientRect();
+
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.style.visibility = "visible";
+    element.style.animation = "slide-up 1s";
+  }
+});
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".footer-content");
+  var position = element.getBoundingClientRect();
+
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.style.visibility = "visible";
+    element.style.animation = "slide-up 1s";
+  }
+});
+
