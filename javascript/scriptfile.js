03fc037c79df5c017204c5d8dd0dbba34407b3f6
sabby <sahibpreet82000@gmail.com>
2021-09-08 19:05:10 +0530

    error resoved

--- javascript/scriptfile.js
+++ javascript/scriptfile.js
@@ -95,13 +95,3 @@ window.addEventListener("scroll", function () {
     element.style.animation = "slide-up 1s";
   }
 });
-window.addEventListener("scroll", function () {
-  var element = document.querySelector(".section-content");
-  var position = element.getBoundingClientRect();
-
-  // checking for partial visibility
-  if (position.top < window.innerHeight && position.bottom >= 0) {
-    element.style.visibility = "visible";
-    element.style.animation = "slide-up 1s";
-  }
-});
--- javascript/temporary.js
+++ javascript/temporary.js
@@ -102,3 +102,14 @@
 
 //   }
 //   obj1.speak();
+
+window.addEventListener("scroll", function () {
+    var element = document.querySelector(".section-content");
+    var position = element.getBoundingClientRect();
+  
+    // checking for partial visibility
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+      element.style.visibility = "visible";
+      element.style.animation = "slide-up 1s";
+    }
+  });
