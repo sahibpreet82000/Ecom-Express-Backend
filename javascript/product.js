9833ef673527cd31a349cc82f4314148e774b3ce
sabby <sahibpreet82000@gmail.com>
2021-09-08 18:32:11 +0530

    slight changes

--- javascript/product.js
+++ javascript/product.js
@@ -291,13 +291,4 @@ function remove(){
 }
 
 // for animation 
-window.addEventListener("scroll", function () {
-  var element = document.querySelector(".section-content");
-  var position = element.getBoundingClientRect();
 
-  // checking for partial visibility
-  if (position.top < window.innerHeight && position.bottom >= 0) {
-    element.style.visibility = "visible";
-    element.style.animation = "slide-up 1s";
-  }
-});
--- javascript/scriptfile.js
+++ javascript/scriptfile.js
@@ -95,3 +95,13 @@ window.addEventListener("scroll", function () {
     element.style.animation = "slide-up 1s";
   }
 });
+window.addEventListener("scroll", function () {
+  var element = document.querySelector(".section-content");
+  var position = element.getBoundingClientRect();
+
+  // checking for partial visibility
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    element.style.visibility = "visible";
+    element.style.animation = "slide-up 1s";
+  }
+});
