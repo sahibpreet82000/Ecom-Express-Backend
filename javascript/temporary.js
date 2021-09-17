93f51f530891a5d2b03d2e31ff470e407f7e1024
sabby <sahibpreet82000@gmail.com>
2021-09-18 01:15:21 +0530

    added changes

--- javascript/temporary.js
+++ javascript/temporary.js
@@ -110,5 +110,3 @@ window.addEventListener("scroll", function () {
     element.style.animation = "slide-up 1s";
   }
 });
-var nm = ['car','jeep','van'];
-let set = localStorage.setItem('name',JSON.stringify(nm)) ;
