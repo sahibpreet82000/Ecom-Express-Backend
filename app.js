0e2bc103b8961461d158409c67b23ae970bcd7ca
sabby <sahibpreet82000@gmail.com>
2021-10-18 23:47:25 +0530

    Database Created

--- app.js
+++ app.js
@@ -2,27 +2,54 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 80;
+const mongoose = require("mongoose");
 
-//EXPRESS FILES
-app.use('/static',express.static('static'))
-app.use(express.urlencoded())
+// Initializing Mongoose
 
-//PUG SPECIFIC STUFF
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/registeration");
+}
+
+// mongoose Schema
+
+const registerationSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  confirmpassword: String,
+});
+const registerSchema = mongoose.model("registerations", registerationSchema);
+
+// EXPRESS FILES
+
+app.use("/static", express.static("static"));
+app.use(express.urlencoded());
+
+// PUG SPECIFIC STUFF
+
 // app.set('view engine','pug') //Set the template engine as pug
-app.set('views',path.join(__dirname,'views')) //Set VIews the directory
+app.set("views", path.join(__dirname, "views")); //Set VIews the directory
 
-//End point
+// End point
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/static/html/register.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/static/html/register.html"));
+});
 
-app.post('/register',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/static/html/register.html'));
-  console.log(req.body);
-})
-//Start the Server
+app.post("/register", (req, res) => {
+  var myData = new registerSchema(req.body);
+  myData.save().then(()=>{
+      res.send("You are registered successfully")
+  }).catch(()=>{
+      res.status(404).send("Item was not saved");
+  })
+});
 
-app.listen(port,()=>{
-    console.log("the app is running");
-})
+// Start the Server
+
+app.listen(port, () => {
+  console.log("the app is running");
+});
